fe8eb4a9c4ba52b5fd6af7c12eb32f92635d0f32
mattwhite1995 <matthew.white.13@cnu.edu>
2019-02-03 11:10:06 -0500

    Found table on first page and loaded into data frame

--- RRugby.R
+++ RRugby.R
@@ -1,3 +1,12 @@
 #The intent of this script is to determine if there would be any effect in game 
 #outcomes over rugby history if the different scoring systems had been used
 #throughout history 
+
+install.packages("rvest")
+
+library(rvest)
+
+Scrum <- read_html("http://stats.espnscrum.com/statsguru/rugby/stats/index.html?class=1;orderby=date;template=results;type=team;view=match")
+
+table <- html_table(html_nodes(Scrum, "table")[[4]])
+
