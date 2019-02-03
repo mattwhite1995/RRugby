a2892b2b729443602abe237e4b96ae9e49d39701
mattwhite1995 <matthew.white.13@cnu.edu>
2019-02-03 12:35:49 -0500

    Created for-loop to navigate pages and bind data to multi_table data frame

--- RRugby.R
+++ RRugby.R
@@ -5,8 +5,18 @@
 install.packages("rvest")
 
 library(rvest)
+multi_table <- 1
+for(i in 1:2){ #look to get max page number from page
+  url <- paste("http://stats.espnscrum.com/statsguru/rugby/stats/index.html?class=1;filter=advanced;orderby=date;page=",i,";size=200;template=results;type=team;view=match", sep = '')
+  Scrum <- read_html(url)
+  table <- html_table(html_nodes(Scrum, "table")[[4]])
+  if(typeof(multi_table) != "list"){
+    multi_table <- table[FALSE,]
+  }
+  multi_table <- rbind(multi_table, table)
+}
 
-Scrum <- read_html("http://stats.espnscrum.com/statsguru/rugby/stats/index.html?class=1;orderby=date;template=results;type=team;view=match")
+head(table)
+head(multi_table)
 
-table <- html_table(html_nodes(Scrum, "table")[[4]])
-
+# TBD use to remove NA columns Filter(function(x)!all(is.na(x)), table)
