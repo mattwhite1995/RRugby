53da34c22b8cf7cb814b7b74ed3d891e9322a759
mattwhite1995 <matthew.white.13@cnu.edu>
2019-02-10 10:39:44 -0500

    Added tidyverse, created six nations subset, merged rows so that opponents are on same row

--- RRugby.R
+++ RRugby.R
@@ -3,10 +3,12 @@
 #throughout history 
 
 install.packages("rvest")
+install.packages("tidyverse")
 
 library(rvest)
+library(tidyverse)
 multi_table <- 1
-for(i in 1:2){ #look to get max page number from page
+for(i in 1:30){ #look to get max page number from page
   url <- paste("http://stats.espnscrum.com/statsguru/rugby/stats/index.html?class=1;filter=advanced;orderby=date;page=",i,";size=200;template=results;type=team;view=match", sep = '')
   Scrum <- read_html(url)
   table <- html_table(html_nodes(Scrum, "table")[[4]])
@@ -16,7 +18,13 @@ for(i in 1:2){ #look to get max page number from page
   multi_table <- rbind(multi_table, table)
 }
 
-head(table)
-head(multi_table)
 
-# TBD use to remove NA columns Filter(function(x)!all(is.na(x)), table)
+#remove NA columns
+results <- Filter(function(x)!all(is.na(x)), multi_table)
+colnames(results) <- c("Team","Results","For","Aga","Diff","Tries","Conv","Pens","Drop","GfM","Opponent","Ground","Date")
+six_nations <- c("England","Scotland","Ireland","Wales","Italy","France")
+results2 <- filter(results, Team %in% six_nations)
+row_match <- merge(results2, results2, by = c("Ground","Date"))
+View(filter(row_match, Team.x != Team.y))
+# View(results)
+
